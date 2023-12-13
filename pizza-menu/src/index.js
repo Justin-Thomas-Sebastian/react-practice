6576ae585770870e55f7e3eab3cc8ee9dcf62c3f
Justin Sebastian <justinthomassebastian@gmail.com>
2023-12-12 20:34:22 -0500

    update pizza-menu components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -60,7 +60,7 @@ function App() {
 function Header() {
   return (
     <header className="header">
-      <h1>React Pizza Co.</h1>;
+      <h1>React Pizza Co.</h1>
     </header>
   );
 }
@@ -69,13 +69,62 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <div className="pizzas">
+        <Pizza
+          name="Pizza Spinaci"
+          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+          price={12}
+          photoName="pizzas/spinaci.jpg"
+        />
+
+        <Pizza
+          name="Pizza Funghi"
+          ingredients="Tomato, mozarella, mushrooms, and onion"
+          price={12}
+          photoName="pizzas/funghi.jpg"
+        />
+
+        <Pizza
+          name="Focaccia"
+          ingredients="Bread with italian olive oil and rosemary"
+          price={6}
+          photoName="pizzas/focaccia.jpg"
+        />
+
+        <Pizza
+          name="Pizza Margherita"
+          ingredients="Tomato and mozarella"
+          price={10}
+          photoName="pizzas/margherita.jpg"
+        />
+
+        <Pizza
+          name="Pizza Salamino"
+          ingredients="Tomato, mozarella, and pepperoni"
+          price={15}
+          photoName="pizzas/salamino.jpg"
+        />
+
+        <Pizza
+          name="Pizza Prosciutto"
+          ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+          price={18}
+          photoName="pizzas/prosciutto.jpg"
+        />
+      </div>
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt="Pizza Spinaci"></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+      </div>
+    </div>
   );
 }
 
@@ -84,21 +133,14 @@ function Footer() {
   const openHour = 11;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
+  const openOrClosed = isOpen ? "open" : "closed";
 
   return (
     <footer>
-      <h2>{new Date().toLocaleDateString()}. We are open.</h2>
+      <h2>
+        {new Date().toLocaleDateString()}. We are {openOrClosed}.
+      </h2>
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
