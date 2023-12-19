15ab52f5997594eb491ea78964aea4429b333c1b
Justin Sebastian <justinthomassebastian@gmail.com>
2023-12-19 16:54:33 -0500

    Add functionality to close window

--- steps/src/App.js
+++ steps/src/App.js
@@ -3,29 +3,42 @@ import { useState } from "react";
 const messages = ["Learn React", "Apply for jobs", "Invest your new income"];
 
 export default function App() {
-  const [step, setStep] = useState(1);
-  function handlePrevious() {
-    if (step > 1) setStep(step - 1);
-  }
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
 
-  function handleNext() {
-    if (step < 3) setStep(step + 1);
-  }
+    function handlePrevious() {
+        if (step > 1) setStep(step - 1);
+    }
 
-  return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button onClick={handlePrevious}>Previous</button>
-        <button onClick={handleNext}>Next</button>
-      </div>
-    </div>
-  );
+    function handleNext() {
+        if (step < 3) setStep(step + 1);
+    }
+
+    function handleClose() {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <>
+            <button className="close" onClick={handleClose}>
+                X
+            </button>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
+                    <div className="buttons">
+                        <button onClick={handlePrevious}>Previous</button>
+                        <button onClick={handleNext}>Next</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
