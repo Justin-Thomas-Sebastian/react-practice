3e9ac181214ffa8ddc455382cbfedaba6394b0f0
Justin Sebastian <justinthomassebastian@gmail.com>
2024-01-25 13:38:02 -0500

    init travel-list project

---
+++ travel-list/src/App.js
@@ -0,0 +1,61 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Shoes", quantity: 2, packed: true },
+    { id: 4, description: "Bags", quantity: 2, packed: false },
+];
+
+export default function App() {
+    return (
+        <div className="app">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
+}
+
+function Logo() {
+    return <h1>🏝️ Far Away 💼</h1>;
+}
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip?</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} key={item.id} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+        </li>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em>
+                You have X items in your list and you have already packed Y (Z
+                %)
+            </em>
+        </footer>
+    );
+}
